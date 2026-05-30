31bf35c5a8903403ef8fc56d4a6f4cabd60a7e1a
Noah Lerner <126131243+Lernej@users.noreply.github.com>
2026-05-30 19:08:05 -0400

    Initial commit

---
+++ chessBackend.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import shutil
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+	return {"mesage" : "Hello World!"}
+
+@app.post("/photo")
+async def recieve_photo(file: UploadFile = File(...)):
+	if file.content_type not in ["image/jpeg", "image/jpg"]:
+		raise HTTPException(status_code=400, detail="File must be a JPEG image")
+	
+	file_location = f"uploaded_{file.filename}"
+
+	with open(file_location, "wb+") as file_object:
+		shutil.copyfileobj(file.file, file_object)
+
+	return {"info": f"File '{file.filename}' saved at '{file_location}'"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("chessBackend:app", host="0.0.0.0", port=8000, reload=True)
